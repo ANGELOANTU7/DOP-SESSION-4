f6fd66034f2fcc0146a0b98aa82ed2e0de7f7b22
Angelo Antu <94218410+ANGELOANTU7@users.noreply.github.com>
2023-01-12 17:06:27 +0530

    Update PIR.ino

--- PIR.ino
+++ PIR.ino
@@ -18,19 +18,9 @@ void loop(){
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
     digitalWrite(ledPin, HIGH);  // turn LED ON
-    if (pirState == LOW) {
-      // we have just turned on
-      Serial.println("Motion detected!");
-      // We only want to print on the output change, not state
-      pirState = HIGH;
-    }
+    
   } else {
     digitalWrite(ledPin, LOW); // turn LED OFF
-    if (pirState == HIGH){
-      // we have just turned of
-      Serial.println("Motion ended!");
-      // We only want to print on the output change, not state
-      pirState = LOW;
-    }
+
   }
 }
